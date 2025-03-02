3eb68ab8e59f9d0e7376a25c2e3fba80bd763a67
quguiyou <quguiyou1076@qq.com>
2025-03-02 16:44:34 +0800

    draw-tildes

--- src/editor.rs
+++ src/editor.rs
@@ -30,6 +30,12 @@ impl Editor {
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+        if self.should_quit {
+            println!("Goodbye.\r");
+        } else {
+            self.draw_rows();
+            print!("{}", termion::cursor::Goto(1, 1));
+        }
         io::stdout().flush()
     }
 
@@ -40,6 +46,12 @@ impl Editor {
             _ => (),
         }
         Ok(())
+    }
+
+    fn draw_rows(&self) {
+        for _ in 0..24 {
+            println!("~\r");
+        }
     }
 }
 
@@ -52,5 +64,6 @@ fn read_key() -> Result<Key, std::io::Error> {
 }
 
 fn die(e: std::io::Error) {
+    print!("{}", termion::clear::All);
     panic!("{}", e);
 }
