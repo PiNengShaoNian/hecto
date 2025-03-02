71bc8e4311e6bb5bbf2bfc4a16989c9ac288c47c
quguiyou <quguiyou1076@qq.com>
2025-03-02 15:58:51 +0800

    die-on-input-error

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,19 @@ fn die(e: std::io::Error) {
 fn main() {
     enable_raw_mode().unwrap();
     for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
-        if c.is_control() {
-            println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-        } else {
-            println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
-        }
-        if b == to_ctrl_byte('q') {
-            break;
+        match b {
+            Ok(b) => {
+                let c = b as char;
+                if c.is_control() {
+                    println!("{:?} \r", b);
+                } else {
+                    println!("{:?} ({})\r", b, c);
+                }
+                if b == to_ctrl_byte('q') {
+                    break;
+                }
+            }
+            Err(err) => die(err),
         }
     }
 }
