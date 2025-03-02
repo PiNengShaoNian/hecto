2fa6fc84dd2080ced90181bb0e7f865d1096fe46
quguiyou <quguiyou1076@qq.com>
2025-03-02 15:55:18 +0800

    ctrl-q-to-quit

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ use std::io::{self, Read};
 
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b0001_1111
+}
+
 fn main() {
     enable_raw_mode().unwrap();
     for b in io::stdin().bytes() {
@@ -12,8 +17,7 @@ fn main() {
         } else {
             println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
         }
-        if c == 'q' {
-            disable_raw_mode().unwrap();
+        if b == to_ctrl_byte('q') {
             break;
         }
     }
