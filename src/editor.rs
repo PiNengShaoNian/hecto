45100886392c5738caaa6851d4f19a258f1f3619
quguiyou <quguiyou1076@qq.com>
2025-03-06 23:15:45 +0800

    add-offset

--- src/editor.rs
+++ src/editor.rs
@@ -15,6 +15,7 @@ pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
     cursor_position: Position,
+    offset: Position,
     document: Document,
 }
 
@@ -46,6 +47,7 @@ impl Editor {
             terminal: Terminal::default().expect("Failed to initialize terminal"),
             document,
             cursor_position: Position::default(),
+            offset: Position::default(),
         }
     }
 
